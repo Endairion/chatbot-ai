cd1d9d446ef16ec63d83d277f9428e00157d2b34
Endairion <pmliew@gmail.com>
2024-07-12 11:04:48 +0800

    Added llama parse and proper splitting techniques

--- src/chatbot_app/embedding_function.py
+++ src/chatbot_app/embedding_function.py
@@ -1,9 +1,18 @@
 from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_embedding_function():
-
-    embedding = OpenAIEmbeddings(model="text-embedding-3-large")
+    model_name = "intfloat/multilingual-e5-large"
+    model_kwargs = {'device': 'cpu'}
+    embedding = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    )
     return embedding
 
 
-
+if __name__ == "__main__":
+    embedding = get_embedding_function()
+    text = "Hello, how are you?"
+    query = embedding.embed_query(text)
+    print(query)
